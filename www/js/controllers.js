c82ecbc4782ff6fbbd21495de74051f06b49e92b
Azz-Eddine DJEKMANI <azzeddine.djekmani@um5s.net.ma>
2015-05-26 20:59:20 +0100

    no message

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,7 +34,8 @@ angular.module('starter.controllers', [])
       url: 'http://auth0-nodejsapi-sample.herokuapp.com/secured/ping',
       method: 'GET'
     }).then(function() {
-      alert("We got the secured data successfully");
+        $scope.next = 0;
+
     }, function() {
       alert("Please download the API seed so that you can call it.");
     });
